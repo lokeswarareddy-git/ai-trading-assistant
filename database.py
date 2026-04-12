dad5abb16ccfa577b00ce8fc2c88a4fa483a76ff
Lokeswara Reddy Valipireddy <lokeswarareddy@gmail.com>
2026-04-12 13:44:20 -0700

    updated database engine got postgress migration

--- database.py
+++ database.py
@@ -3,7 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 #DATABASE_URL = "sqlite:///./trades.db"
 DATABASE_URL = "postgresql://trading_db_awmn_user:omg3kqkY9rtjpcx9ceQeyQPBLXg1FWbY@dpg-d7e01mvlk1mc73f2aju0-a/trading_db_awmn"
-engine = create_engine(DATABASE_URL, connect_args = {"check_same_thread": False})
+
+#engine = create_engine(DATABASE_URL, connect_args = {"check_same_thread": False}) # Commenting as we are updating as part of mysql to postgress migration
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+) # adding for postgress
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
