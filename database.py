bb25037bd634aa4130f1f2f6627a45676474d553
Lokeswara Reddy Valipireddy <lokeswarareddy@gmail.com>
2026-04-13 12:37:20 -0700

    Trying to fix issue with frequest disconnetion

--- database.py
+++ database.py
@@ -5,10 +5,19 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "postgresql://trading_db_awmn_user:omg3kqkY9rtjpcx9ceQeyQPBLXg1FWbY@dpg-d7e01mvlk1mc73f2aju0-a/trading_db_awmn"
 
 #engine = create_engine(DATABASE_URL, connect_args = {"check_same_thread": False}) # Commenting as we are updating as part of mysql to postgress migration
+"""
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 ) # adding for postgress
+"""
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=10,
+    max_overflow=20
+)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
