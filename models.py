6b8c812ebd0ee8ba44834b710972e3be33f1b21e
Lokeswara Reddy Valipireddy <lokeswarareddy@gmail.com>
2026-04-15 14:18:10 -0700

    added created at

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
 
 
 class Trade(Base):
